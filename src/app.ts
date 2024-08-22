6224b33d12b8c8e61b7c07880943a87c4f3e96cd
Akaczek <luk12349876@o2.pl>
2024-08-22 18:14:36 +0200

    Update script

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,31 @@
 import { generateMaps } from './generateMaps';
 import { flattenMap } from './utils/flattenMap';
 
-const HOW_MANY = 10;
-const SIZE = 20;
+if (process.argv.length < 4) {
+  console.log('Usage: node dist/app.js <how-many> <size>');
+  process.exit(1);
+}
+
+if (isNaN(parseInt(process.argv[2])) || isNaN(parseInt(process.argv[3]))) {
+  console.log('Arguments must be integers');
+  process.exit(1);
+}
+
+if (parseInt(process.argv[2]) < 1 || parseInt(process.argv[3]) < 1) {
+  console.log('Arguments must be greater than 0');
+  process.exit(1);
+}
+
+const start = performance.now();
+
+const HOW_MANY = parseInt(process.argv[2]);
+const SIZE = parseInt(process.argv[3]);
 
 generateMaps(HOW_MANY, SIZE);
 
 flattenMap();
+
+const end = performance.now();
+
+console.log(`Size of each map: ${SIZE}`);
+console.log(`Execution time: ${end - start} ms`);
